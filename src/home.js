1daa6da6e2d854adb6e562d6e07524b62583d878
rizk <rizkdotai@gmail.com>
2023-12-01 12:35:20 +0200

    Add basic html structure

--- src/home.js
+++ src/home.js
@@ -1,10 +1,20 @@
 const createHomePage = () => {
-    const content = document.querySelector('#content');
-    const header = document.createElement('header');
-    header.classList.add('bg-blue-500', 'text-white', 'py-4', 'col-span-2');
-    header.textContent = 'DUI Interactions'
-
-    content.appendChild(header);
+   
 }
 
 export default createHomePage
+
+/*
+
+ const content = document.querySelector('#content');
+    const header = document.createElement('div');
+    const nav = document.createElement('div')
+
+    header.classList.add('bg-blue-500', 'text-white', 'py-4', 'col-span-2', 'primary-header');
+    header.textContent = 'DUI Interactions'; 
+    nav.classList.add('nav-wrapper') 
+
+
+    content.appendChild(header);
+
+*/
